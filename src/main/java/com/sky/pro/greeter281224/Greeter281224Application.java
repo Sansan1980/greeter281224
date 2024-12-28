bfb2bceb7db15c6834cfafda05888d89b5c2c0f7
Homework-10 <danilovsansan@gmail.com>
2024-12-28 04:57:42 +0300

    initial commit

---
+++ src/main/java/com/sky/pro/greeter281224/Greeter281224Application.java
@@ -0,0 +1,13 @@
+package com.sky.pro.greeter281224;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greeter281224Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Greeter281224Application.class, args);
+	}
+
+}
